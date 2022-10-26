0808b2d600703df7d94546a8bbc8095fcd137319
Bilkhoo, Aman (k21013503) <aman.bilkhoo@kcl.ac.uk>
2022-10-26 09:36:54 +0100

    Add model

--- things/models.py
+++ things/models.py
@@ -1,3 +1,9 @@
+from webbrowser import MacOSX
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+class Thing(models.Model):
+    name=models.CharField(blank=False, unique=True, max_length=30)
+    description=models.CharField(blank=True, max_length=120)
+    quantity=models.IntegerField(validators=[MinValueValidator(0, 'Min quantity is 0'), MaxValueValidator(100, 'Max quantity is 100')])
